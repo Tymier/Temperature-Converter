9042a5b0a81c73d181258067bc3606d826946afb
Tymier Baskerville <56207704+Tymier@users.noreply.github.com>
2021-04-14 14:27:07 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
+
+function roundNum(num){
+    return Math.round(num*100)/100;
+}
+
+function celciusConverted() {
+    const cTemp = parseFloat(celciusInput.value);
+    const fTemp = (cTemp * (9/5)) + 32;
+    const kTemp = cTemp + 273.15;
+    fahrenheitInput.value = roundNum(fTemp);
+    kelvinInput.value = roundNum(kTemp);
+}
+
+function fahrenheitConverted() {
+    const fTemp = parseFloat(fahrenheitInput.value);
+    const cTemp = (fTemp - 32) * (5/9);
+    const kTemp = (fTemp + 459.67) + 5/9;
+    celciusInput.value = roundNum(cTemp);
+    kelvinInput.value = roundNum(kTemp);
+}
+
+function kelvinConverted() {
+    const kTemp = parseFloat(kelvinInput.value);
+    const cTemp = kTemp - 273.15;
+    const fTemp = 9/5 * (kTemp - 273) * 32;
+    celciusInput.value = roundNum(cTemp);
+    fahrenheitInput.value = roundNum(fTemp);
+}
+
+function main() {
+celciusInput.addEventListener('input', celciusConverted);
+fahrenheitInput.addEventListener('input', fahrenheitConverted);
+kelvinInput.addEventListener('input', kelvinConverted);
+}
+
+main();
+
+
+
+
+
+
+
+
+
